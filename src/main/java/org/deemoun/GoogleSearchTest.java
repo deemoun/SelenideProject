e4632ad8be21747d19703572ffbbf608de504c26
Dmitry Y. <deemoun@users.noreply.github.com>
2024-02-17 00:39:34 +0200

    Fixing the search test for duckduckgo.com

--- src/main/java/org/deemoun/GoogleSearchTest.java
+++ src/main/java/org/deemoun/GoogleSearchTest.java
@@ -15,9 +15,8 @@ public class GoogleSearchTest {
 
     @Test
     public void userCanSearch() {
-        open("https://google.com"); // Открыть Google
+        open("https://duckduckgo.com"); // Открыть Google
         // Добавить cookies для обхода Google consent
-        executeJavaScript("document.cookie = 'CONSENT=YES+cb.20210328-17-p0.en+FX+941;'");
 
         $("[name=q]").setValue("Selenide").pressEnter(); // Ввести "Selenide" в поиск и нажать Enter
         $$(".g").first().shouldHave(text("Selenide: concise UI tests in Java")); // Проверить, что первый результат содержит текст
